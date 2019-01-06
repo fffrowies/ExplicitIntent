68da004abb1448ed27990102d1a2ca400aecb3a6
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-06 08:28:07 -0300

    Initial Project Setup

--- app/src/main/java/com/fffrowies/explicitintent/MainActivity.java
+++ app/src/main/java/com/fffrowies/explicitintent/MainActivity.java
@@ -2,12 +2,28 @@ package com.fffrowies.explicitintent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button submitBtn;
+    private EditText nameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submitBtn = findViewById(R.id.submitBtn);
+        nameEditText = findViewById(R.id.nameEditText);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/fffrowies/explicitintent/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.fffrowies.explicitintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        TextView textView = findViewById(R.id.textView);
+    }
+}
